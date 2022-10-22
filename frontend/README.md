8edd8256e1fa41622b8ed23dcc0b4a4a805b27fe
Faris Durrani <afarisdurrani@gmail.com>
2022-10-22 11:34:53 -0400

    React implemented

---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+A chrome extension example written in react for this youtube video:
+https://www.youtube.com/watch?v=VP7q-c9_is8
+
+Use it by running npm build and adding the build folder as an unpacked extension.
---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  function test(){
+    /* eslint-disable no-undef */
+    chrome.tabs.query({active: true, currentWindow:true}, tabs=>{
+      const activeTabId = tabs[0].id;
+      chrome.scripting.executeScript(
+        {
+          target: {tabId: activeTabId},
+          //function: ()=>alert("React chrome extension alert")
+          function: ()=>{document.body.innerHTML = "Hello World"; console.log(2323); alert("3723");}
+        }
+      )
+    })
+  }
+
+  return (
+    <div className="App">
+      <button onClick={test}>Send alert</button>
+    </div>
+  );
+}
+
+export default App;
