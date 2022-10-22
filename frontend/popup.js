8c6bc4d7971bafa639a04afba03f820fdbbf8803
Faris Durrani <afarisdurrani@gmail.com>
2022-10-22 10:37:14 -0400

    Add files via upload

---
+++ frontend/popup.js
@@ -0,0 +1,18 @@
+console.log("This is a popup!");
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var checkPageButton = document.getElementById("clickIt");
+    checkPageButton.addEventListener(
+      "click",
+      function () {
+        chrome.tabs.getSelected(null, function (tab) {
+          alert("Hello..! It's my first chrome extension.");
+        });
+      },
+      false
+    );
+  },
+  false
+);
